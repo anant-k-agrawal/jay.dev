9dec8875dadebdae85d87a4c9bcdd6ee75e3636d
jay gray <jaygray0919@gmail.com>
2024-03-07 08:17:11 -0600

    reverted to JS comments

--- platform/lib/platform.js
+++ platform/lib/platform.js
@@ -27,9 +27,8 @@ const subdomain = require('./middleware/subdomain.js');
 const webSocketServer = require('@examples/socket-server/socket-server');
 
 const routers = {
-/* 2024/03/07
-  boilerplate: require('../../boilerplate/backend/'),
-*/
+// 2024/03/07
+//  boilerplate: require('../../boilerplate/backend/'),
   cspReport: require('@lib/routers/cspReport.js'),
   example: {
     api: require('@examples'),
@@ -47,15 +46,13 @@ const routers = {
   log: require('@lib/routers/runtimeLog.js'),
   notFound: require('@lib/routers/notFound.js'),
   // packager: require('@lib/routers/packager.js'),
-/* 2024/03/07
-  pixi: require('../../pixi/backend/'),
-  playground: require('../../playground/backend/'),
-*/
+//  2024/03/07
+//  pixi: require('../../pixi/backend/'),
+//  playground: require('../../playground/backend/'),
   search: require('@lib/routers/search.js'),
   static: require('@lib/routers/static.js'),
-/* 2024/03/07
-  survey: require('@lib/routers/surveyComponent.js'),
-*/
+//  2024/03/07
+//  survey: require('@lib/routers/surveyComponent.js'),
   templates: require('@lib/routers/templates.js'),
   thumbor: require('@lib/routers/thumbor.js').thumborRouter,
   whoAmI: require('@lib/routers/whoAmI.js'),
@@ -182,11 +179,10 @@ class Platform {
     this.server.use(routers.whoAmI);
     this.server.use(routers.healthCheck);
     this.server.use(routers.example.api);
-/* 2024/03/07
-    this.server.use(routers.pixi);
-    this.server.use(routers.survey);
-    this.server.use(routers.boilerplate);
-*/
+//    2024/03/07
+//    this.server.use(routers.pixi);
+//    this.server.use(routers.survey);
+//    this.server.use(routers.boilerplate);
     this.server.use(routers.search);
     this.server.use(routers.static);
     this.server.use(routers.templates);
