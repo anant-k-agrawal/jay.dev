a7306fa6556013c09e1f035550921df2c1cd785d
jay gray <jaygray0919@gmail.com>
2024-03-11 03:47:11 -0500

    deprecate pixi

--- gulpfile.js/build.js
+++ gulpfile.js/build.js
@@ -42,7 +42,7 @@ const {whoAmI} = require('./whoAmI.js');
 // const importAdVendorList = require('./import/importAdVendorList.js');
 const {thumborImageIndex} = require('./thumbor.js');
 const CleanCSS = require('clean-css');
-const {PIXI_CLOUD_ROOT} = require('@lib/utils/project').paths;
+// const {PIXI_CLOUD_ROOT} = require('@lib/utils/project').paths;
 const {copyFile} = require('fs/promises');
 // const nunjucks = require('nunjucks');
 // const {importBlog} = require('@lib/templates/ImportBlogFilter.js');
@@ -187,19 +187,19 @@ function buildFrontend(done) {
  * Builds Pixi
  * @return {Promise}
  */
-async function buildPixi() {
-  await sh('mkdir -p pixi/dist');
-  return sh('npm run build:pixi');
-}
+// async function buildPixi() {
+//   await sh('mkdir -p pixi/dist');
+//   return sh('npm run build:pixi');
+// }
 
 /**
  * Builds the pixi cloud functions project
  */
-function buildPixiFunctions() {
-  return sh('npm install', {
-    workingDir: PIXI_CLOUD_ROOT,
-  });
-}
+// function buildPixiFunctions() {
+//   return sh('npm install', {
+//     workingDir: PIXI_CLOUD_ROOT,
+//   });
+// }
 
 /**
  * Builds the boilerplate generator
@@ -289,7 +289,7 @@ function buildPrepare(done) {
     gulp.parallel(
       // buildPlayground,
       // buildBoilerplate,
-      buildPixi,
+      // buildPixi,
       buildFrontend21,
       importAll,
       zipTemplates
@@ -784,7 +784,7 @@ exports.templates = templates;
 exports.importAll = importAll;
 exports.importComponents = importComponents;
 // exports.buildPlayground = buildPlayground;
-exports.buildPixi = buildPixi;
+// exports.buildPixi = buildPixi;
 // exports.buildBoilerplate = buildBoilerplate;
 exports.buildFrontend = buildFrontend;
 exports.buildSamples = buildSamples;
@@ -796,7 +796,7 @@ exports.staticify = staticify;
 exports.unpackArtifacts = unpackArtifacts;
 exports.collectStatics = collectStatics;
 exports.whoAmI = whoAmI;
-exports.buildPixiFunctions = buildPixiFunctions;
+// exports.buildPixiFunctions = buildPixiFunctions;
 exports.buildFinalize = gulp.series(
   gulp.parallel(collectStatics, persistBuildInfo),
   thumborImageIndex
